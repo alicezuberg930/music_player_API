7195403c8b1517736e580c4f1bc9b9169d0ac7bf
tien.nguyenvinh <tien.nguyenvinh@di4l.vn>
2024-10-01 12:42:46 +0700

    add new api

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const { default: axios } = require('axios')
 const express = require('express')
-// const ZingMp3 = require("zingmp3-api")
-// const { ZingMp3 } = require("zingmp3-api-full-v3")
-const { ZingMp3 } = require("zingmp3-api-full")
-
+const cors = require('cors')
+const ZingMp3 = require('./zingmp3.js')
 const app = express()
-const port = 3000
-const hostname = "127.0.0.1"
+
+require('dotenv').config()
+const port = process.env.PORT || 3001
+
+app.use(cors('/api/v2', { origin: ["https://music-player-website-olive.vercel.app", "http://localhost:3000"] }))
 
 app.get('/', async (req, res) => {
-    console.log("deployed")
-    res.json({ aaa: "deployed" })
+    res.send("SERVER DEPLOYED")
 });
 
 app.get('/api/v2/search/multi', async (req, res) => {
     try {
-        const results = await ZingMp3.search(req.query.search);
+        const results = await ZingMp3.search(req.query.query);
         console.log(results);
         res.json(results);
     } catch (error) {
@@ -66,7 +65,7 @@ app.get('/api/v2/page/get/chart-home', async (req, res) => {
 
 app.get('/api/v2/page/get/playlist', async (req, res) => {
     try {
-        const results = await ZingMp3.getDetailPlaylist(req.query.id);
+        const results = await ZingMp3.getDetailsPlaylist(req.query.id);
         res.json(results);
     } catch (error) {
         console.log(error);
@@ -76,13 +75,19 @@ app.get('/api/v2/page/get/playlist', async (req, res) => {
 
 app.get('/api/v2/song/get/streaming', async (req, res) => {
     try {
-        const results = await ZingMp3.getSong(req.query.id);
+        const results = await ZingMp3.getSongStreaming(req.query.id);
         console.log(results);
-        if (results.url != null) {
-            const data = axios.get(results.url);
-            res.json(data);
-            return;
-        }
+        res.json(results);
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
+})
+
+app.get('/api/v2/song/get/info', async (req, res) => {
+    try {
+        const results = await ZingMp3.getInfoSong(req.query.id);
+        console.log(results);
         res.json(results);
     } catch (error) {
         console.log(error);
@@ -106,6 +111,37 @@ app.get('/api/v2/page/get/video', async (req, res) => {
         const results = await ZingMp3.getVideo(req.query.id);
         console.log(results);
         res.json(results);
+    } catch (ecalrror) {
+        console.log(error);
+        res.json(error);
+    }
+})
+
+app.get('/api/v2/page/get/artist', async (req, res) => {
+    try {
+        const results = await ZingMp3.getArtist(req.query.name);
+        console.log(results);
+        res.json(results);
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
+})
+
+app.get('/api/v2/song/get/list', async (req, res) => {
+    try {
+        const results = await ZingMp3.getListArtistSong(req.query.id, req.query.page, req.query.count);
+        res.json(results);
+    } catch (error) {
+        res.json(error);
+    }
+})
+
+app.get('/api/v2/page/get/week-chart', async (req, res) => {
+    try {
+        const results = await ZingMp3.getWeekChart(req.query.id, req.query.week, req.query.year);
+        console.log(results);
+        res.json(results);
     } catch (error) {
         console.log(error);
         res.json(error);
@@ -117,5 +153,5 @@ app.use((req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server running at ${port}/`)
+    console.log(`Server running at http://127.0.0.1:${port}/`)
 })
---
+++ zingmp3.js
@@ -0,0 +1,303 @@
+const axios = require("axios");
+const crypto = require("crypto");
+
+class ZingMp3Api {
+
+    constructor(VERSION, URL, SECRET_KEY, API_KEY, CTIME) {
+        this.VERSION = VERSION;
+        this.URL = URL;
+        this.SECRET_KEY = SECRET_KEY;
+        this.API_KEY = API_KEY;
+        this.CTIME = CTIME;
+    }
+
+    getHash256(str) {
+        return crypto.createHash("sha256").update(str).digest("hex");
+    }
+
+    getHmac512(str, key) {
+        let hmac = crypto.createHmac("sha512", key);
+        return hmac.update(Buffer.from(str, "utf8")).digest("hex");
+    }
+
+    hashParamNoId(path) {
+        return this.getHmac512(
+            path + this.getHash256(`ctime=${this.CTIME}version=${this.VERSION}`),
+            this.SECRET_KEY
+        );
+    }
+
+    hashParam(path, id) {
+        return this.getHmac512(
+            path + this.getHash256(`ctime=${this.CTIME}id=${id}version=${this.VERSION}`),
+            this.SECRET_KEY
+        );
+    }
+
+    hashParamHome(path) {
+        return this.getHmac512(
+            path + this.getHash256(`count=30ctime=${this.CTIME}page=1version=${this.VERSION}`),
+            this.SECRET_KEY
+        );
+    }
+
+    hashCategoryMV(path, id, type) {
+        return this.getHmac512(
+            path + this.getHash256(`ctime=${this.CTIME}id=${id}type=${type}version=${this.VERSION}`),
+            this.SECRET_KEY
+        );
+    }
+
+    hashListMV(path, id, type, page, count) {
+        return this.getHmac512(
+            path +
+            this.getHash256(`count=${count}ctime=${this.CTIME}id=${id}page=${page}type=${type}version=${this.VERSION}`),
+            this.SECRET_KEY
+        );
+    }
+
+    hashWeekChart(path, id, week, year) {
+        return this.getHmac512(
+            path + this.getHash256(`ctime=${this.CTIME}id=${id}version=${this.VERSION}`),
+            this.SECRET_KEY
+        )
+    }
+
+    getCookie = async () => {
+        try {
+            let cookie = await axios.get(this.URL)
+            if (cookie.headers["set-cookie"]) {
+                return cookie.headers["set-cookie"][1]
+            }
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+    }
+
+    requestZingMp3 = async (path, qs) => {
+        try {
+            const client = axios.create({
+                baseURL: `${this.URL}`,
+            });
+            client.interceptors.response.use((res) => res.data);
+            let cookie = await this.getCookie()
+            let data = await client.get(path, {
+                headers: {
+                    Cookie: `${cookie}`,
+                },
+                params: {
+                    ...qs,
+                    ctime: this.CTIME,
+                    version: this.VERSION,
+                    apiKey: this.API_KEY,
+                }
+            })
+            return data
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+    }
+
+    getWeekChart = async (chartId, weekCount, yearCount) => {
+        try {
+            let weekChart = await this.requestZingMp3("/api/v2/page/get/week-chart", {
+                id: chartId,
+                week: weekCount,
+                year: yearCount,
+                sig: this.hashParam("/api/v2/page/get/week-chart", chartId)
+            })
+            return weekChart
+        } catch (error) {
+            return error
+        }
+    }
+
+    getSongStreaming = async (songId) => {
+        try {
+            let song = await this.requestZingMp3("/api/v2/song/get/streaming", {
+                id: songId,
+                sig: this.hashParam("/api/v2/song/get/streaming", songId)
+            })
+            return song
+        } catch (error) {
+            return error
+        }
+    }
+
+    getDetailsPlaylist = async (playlistId) => {
+        try {
+            let playlist = await this.requestZingMp3("/api/v2/page/get/playlist", {
+                id: playlistId,
+                sig: this.hashParam("/api/v2/page/get/playlist", playlistId)
+            })
+            return playlist
+        } catch (error) {
+            return error
+        }
+    }
+
+    getHome = async () => {
+        try {
+            let home = await this.requestZingMp3("/api/v2/page/get/home", {
+                page: 1,
+                segmentId: "-1",
+                count: "30",
+                sig: this.hashParamHome("/api/v2/page/get/home")
+            })
+            return home
+        } catch (error) {
+            return error
+        }
+    }
+
+    getTop100 = async () => {
+        try {
+            const top100 = await this.requestZingMp3("/api/v2/page/get/top-100", {
+                sig: this.hashParamNoId("/api/v2/page/get/top-100")
+            })
+            return top100
+        } catch (error) {
+            return error
+        }
+    }
+
+    getChartHome = async () => {
+        try {
+            let chartHome = await this.requestZingMp3("/api/v2/page/get/chart-home", {
+                sig: this.hashParamNoId("/api/v2/page/get/chart-home")
+            })
+            return chartHome
+        } catch (error) {
+            return error
+        }
+    }
+
+    getNewReleaseChart = async () => {
+        try {
+            const newReleaseChart = await this.requestZingMp3("/api/v2/page/get/newrelease-chart", {
+                sig: this.hashParamNoId("/api/v2/page/get/newrelease-chart")
+            })
+            return newReleaseChart
+        } catch (error) {
+            return error
+        }
+    }
+
+    getInfoSong = async (songId) => {
+        try {
+            let songInfo = await this.requestZingMp3("/api/v2/song/get/info", {
+                id: songId,
+                sig: this.hashParam("/api/v2/song/get/info", songId)
+            })
+            return songInfo
+        } catch (error) {
+            return error
+        }
+    }
+
+    getListArtistSong = async (artistId, page, count) => {
+        try {
+            let artistSongs = await this.requestZingMp3("/api/v2/song/get/list", {
+                id: artistId,
+                type: "artist",
+                page: page,
+                count: count,
+                sort: "new",
+                sectionId: "aSong",
+                sig: this.hashListMV("/api/v2/song/get/list", artistId, "artist", page, count)
+            })
+            return artistSongs
+        } catch (error) {
+            return error
+        }
+    }
+
+    getArtist = async (name) => {
+        try {
+            const artist = await this.requestZingMp3("/api/v2/page/get/artist", {
+                alias: name,
+                sig: this.hashParamNoId("/api/v2/page/get/artist")
+            })
+            return artist
+        } catch (error) {
+            return error
+        }
+    }
+
+    getLyric = async (songId) => {
+        try {
+            const lyric = await this.requestZingMp3("/api/v2/lyric/get/lyric", {
+                id: songId,
+                sig: this.hashParam("/api/v2/lyric/get/lyric", songId)
+            })
+            return lyric
+        } catch (error) {
+            return error
+        }
+    }
+
+    search = async (name) => {
+        try {
+            const search = await this.requestZingMp3("/api/v2/search/multi", {
+                q: name,
+                sig: this.hashParamNoId("/api/v2/search/multi")
+            })
+            return search
+        } catch (error) {
+            return error
+        }
+    }
+
+    getListMV = async (id, page, count) => {
+        try {
+            const listMV = await this.requestZingMp3("/api/v2/video/get/list", {
+                id: id,
+                type: "genre",
+                page: page,
+                count: count,
+                sort: "listen",
+                sig: this.hashListMV("/api/v2/video/get/list", id, "genre", page, count),
+            })
+            return listMV
+        } catch (error) {
+            return error
+        }
+    }
+
+    getCategoryMV = async (id) => {
+        try {
+            const categoryMV = await this.requestZingMp3("/api/v2/genre/get/info", {
+                id: id,
+                type: "video",
+                sig: this.hashCategoryMV("/api/v2/genre/get/info", id, "video"),
+            })
+            return categoryMV
+        } catch (error) {
+            return error
+        }
+    }
+
+    getVideo = async (videoId) => {
+        try {
+            let video = await this.requestZingMp3("/api/v2/page/get/video", {
+                id: videoId,
+                sig: this.hashParam("/api/v2/page/get/video", videoId),
+            })
+            return video
+        } catch (error) {
+            return error
+        }
+    }
+}
+
+const ZingMp3 = new ZingMp3Api(
+    "1.6.34", // VERSION
+    "https://zingmp3.vn", // URL
+    "2aa2d1c561e809b267f3638c4a307aab", // SECRET_KEY
+    "88265e23d4284f25963e6eedac8fbfa3", // API_KEY
+    String(Math.floor(Date.now() / 1000)) // CTIME
+);
+
+module.exports = ZingMp3;
